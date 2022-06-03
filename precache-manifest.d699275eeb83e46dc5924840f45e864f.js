2b1434d03533200f365844ce40444ba955bb1c1a
olegg789 <olegchikelev789789@gmail.com>
2022-06-03 20:31:50 +0300

    Updates

---
+++ precache-manifest.d699275eeb83e46dc5924840f45e864f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "339c3365e294d00a25b7",
+    "url": "./static/css/main.ae026526.chunk.css"
+  },
+  {
+    "revision": "339c3365e294d00a25b7",
+    "url": "./static/js/main.44b074c2.chunk.js"
+  },
+  {
+    "revision": "c0fef86850209828a77b",
+    "url": "./static/js/runtime~main.cd8dcf17.js"
+  },
+  {
+    "revision": "94586cd7ba07671b8b7b",
+    "url": "./static/css/2.65c7f6b1.chunk.css"
+  },
+  {
+    "revision": "94586cd7ba07671b8b7b",
+    "url": "./static/js/2.c237fe9a.chunk.js"
+  },
+  {
+    "revision": "5c0dd66957079b838938",
+    "url": "./static/js/3.327258a6.chunk.js"
+  },
+  {
+    "revision": "ba31ba7247028e39f021",
+    "url": "./static/js/4.66e5a88d.chunk.js"
+  },
+  {
+    "revision": "a064be45e970aab855e101bcc3b4b945",
+    "url": "./index.html"
+  }
+];
