3d013c2cca3b5a2fd484580057b90ede94440859
olegg789 <olegchikelev789789@gmail.com>
2022-06-06 17:41:09 +0300

    Updates

---
+++ precache-manifest.a8b60f140bf24bc6e12ea54fba69fb35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "104ef0b03af42e9ad947",
+    "url": "./static/css/main.1d898735.chunk.css"
+  },
+  {
+    "revision": "104ef0b03af42e9ad947",
+    "url": "./static/js/main.420426b3.chunk.js"
+  },
+  {
+    "revision": "e3a967461d3a0e0bdd0d",
+    "url": "./static/js/runtime~main.6ecb6fbc.js"
+  },
+  {
+    "revision": "3eb82a429e3525fa6e51",
+    "url": "./static/css/2.65c7f6b1.chunk.css"
+  },
+  {
+    "revision": "3eb82a429e3525fa6e51",
+    "url": "./static/js/2.214b97aa.chunk.js"
+  },
+  {
+    "revision": "d91a016dfcafa17d5dbb",
+    "url": "./static/js/3.89b17c11.chunk.js"
+  },
+  {
+    "revision": "4d3f031cd51edc306807",
+    "url": "./static/js/4.8fc962fe.chunk.js"
+  },
+  {
+    "revision": "8a9fdefad9f84b91633fb55cc07e5ea8",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ea8dd2405271dffaa2a07f79c42a3904.js"
+  "./precache-manifest.a8b60f140bf24bc6e12ea54fba69fb35.js"
 );
 
 workbox.clientsClaim();
